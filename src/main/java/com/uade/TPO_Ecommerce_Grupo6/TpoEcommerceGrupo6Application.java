d08b25836673f2eea34459315d54dc3fb3948ebb
Maria Sol Gomez <48770634+MG180698@users.noreply.github.com>
2026-08-19 12:08:16 -0300

    creo proyecto

---
+++ src/main/java/com/uade/TPO_Ecommerce_Grupo6/TpoEcommerceGrupo6Application.java
@@ -0,0 +1,13 @@
+package com.uade.TPO_Ecommerce_Grupo6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpoEcommerceGrupo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpoEcommerceGrupo6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uade/TPO_Ecommerce_Grupo6/TpoEcommerceGrupo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.TPO_Ecommerce_Grupo6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpoEcommerceGrupo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
