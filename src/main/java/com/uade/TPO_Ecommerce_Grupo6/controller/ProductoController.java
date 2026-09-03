b1a807fb62ac028a3e620af84995fa472d39b60b
luzbarrientos <luzbar206@gmail.com>
2026-09-03 16:21:51 -0300

    feat(producto): agrego endpoint para eliminar productos

--- src/main/java/com/uade/TPO_Ecommerce_Grupo6/controller/ProductoController.java
+++ src/main/java/com/uade/TPO_Ecommerce_Grupo6/controller/ProductoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,5 +68,15 @@ public class ProductoController {
             @Valid @RequestBody ProductoRequest request) {
 
         return ResponseEntity.ok(productoService.actualizar(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(
+            @PathVariable Long id,
+            @RequestParam Long usuarioId) {
+
+        productoService.eliminar(id, usuarioId);
+
+        return ResponseEntity.noContent().build();
     }
 }
