0a03011b849f85f98cbd14b0948ed845dcb78b72
Maria Sol Romero Gomez <MGomez.IBM@supervielle.com>
2026-09-08 00:27:44 -0300

    fix(exception): actualizar UsuarioNoEncontradoException con constructor String

--- src/main/java/com/uade/TPO_Ecommerce_Grupo6/exception/UsuarioNoEncontradoException.java
+++ src/main/java/com/uade/TPO_Ecommerce_Grupo6/exception/UsuarioNoEncontradoException.java
@@ -1,8 +1,13 @@
 package com.uade.TPO_Ecommerce_Grupo6.exception;
-
 import jakarta.validation.constraints.NotNull;
 
 public class UsuarioNoEncontradoException extends RuntimeException {
+    
     public UsuarioNoEncontradoException(@NotNull(message = "El usuario es obligatorio") Long usuarioId) {
+        super("No existe usuario con ID: " + usuarioId);
+    }
+
+    public UsuarioNoEncontradoException(String mensaje) {
+        super(mensaje);
     }
 }
